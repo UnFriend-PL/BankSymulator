30787fdb10c81ccf6a4b772d3de7e23d52a702fe
UnFriend-PL <szymonecki1233@gmail.com>
2024-03-09 19:48:44 +0100

    added scss file with colors

--- banksymulatorclient/src/Components/AccountComponent/DepositModalComponent/Deposit.jsx
+++ banksymulatorclient/src/Components/AccountComponent/DepositModalComponent/Deposit.jsx
@@ -2,22 +2,25 @@ import React, { useContext, useState } from "react";
 import "./Deposit.scss";
 import { NotificationContext } from "../../../Providers/NotificationProvider/NotificationProvider";
 import apiService from "../../../Services/ApiService";
-
+import { UserContext } from "../../../Providers/UserProvider/UserContext";
 function Deposit({ onClose, accountNumber }) {
   const { showNotification } = useContext(NotificationContext);
+  const [isAccountOwner, setIsAccountOwner] = useState(true);
+  const { getUser } = useContext(UserContext);
+  const user = getUser();
   const [formData, setFormData] = useState({
     accountNumber: accountNumber,
     amount: 0,
     contributor: {
-      name: "",
-      surname: "",
-      address: "",
-      email: "",
-      phoneNumber: "",
-      pesel: "",
+      name: user.name,
+      surname: user.surname,
+      address: user.address,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      pesel: user.pesel,
     },
   });
-
+  console.log(formData);
   const handleChange = (e) => {
     const { name, value } = e.target;
     if (Object.keys(formData.contributor).includes(name)) {
@@ -68,42 +71,89 @@ function Deposit({ onClose, accountNumber }) {
           placeholder="Amount"
           min={0}
         />
-        <label htmlFor="name">Contributor Name:</label>
-        <input
-          name="name"
-          onChange={handleChange}
-          placeholder="Contributor Name"
-        />
-        <label htmlFor="surname">Contributor Surname:</label>
-        <input
-          name="surname"
-          onChange={handleChange}
-          placeholder="Contributor Surname"
-        />
-        <label htmlFor="address">Contributor Address:</label>
-        <input
-          name="address"
-          onChange={handleChange}
-          placeholder="Contributor Address"
-        />
-        <label htmlFor="email">Contributor Email:</label>
-        <input
-          name="email"
-          onChange={handleChange}
-          placeholder="Contributor Email"
-        />
-        <label htmlFor="phoneNumber">Contributor Phone Number:</label>
-        <input
-          name="phoneNumber"
-          onChange={handleChange}
-          placeholder="Contributor Phone Number"
-        />
-        <label htmlFor="pesel">Contributor PESEL:</label>
-        <input
-          name="pesel"
-          onChange={handleChange}
-          placeholder="Contributor PESEL"
-        />
+        <label htmlFor="accountOwner">
+          <input
+            type="checkbox"
+            id="accountOwner"
+            checked={isAccountOwner}
+            onChange={() => setIsAccountOwner(!isAccountOwner)}
+          />
+          Account Owner
+        </label>
+        {!isAccountOwner && (
+          <>
+            <label htmlFor="name">Contributor Name:</label>
+            <input
+              name="name"
+              onChange={handleChange}
+              placeholder="Contributor Name"
+              value={formData.contributor.name}
+            />
+            <label htmlFor="surname">Contributor Surname:</label>
+            <input
+              name="surname"
+              onChange={handleChange}
+              placeholder="Contributor Surname"
+              value={formData.contributor.surname}
+            />
+            <label htmlFor="address">Contributor Address:</label>
+            <input
+              name="address"
+              onChange={handleChange}
+              placeholder="Contributor Address"
+              value={formData.contributor.address}
+            />
+            <label htmlFor="email">Contributor Email:</label>
+            <input
+              name="email"
+              onChange={handleChange}
+              placeholder="Contributor Email"
+              value={formData.contributor.email}
+            />
+            <label htmlFor="phoneNumber">Contributor Phone Number:</label>
+            <input
+              name="phoneNumber"
+              onChange={handleChange}
+              placeholder="Contributor Phone Number"
+              value={formData.contributor.phoneNumber}
+            />
+            <label htmlFor="pesel">Contributor PESEL:</label>
+            <input
+              name="pesel"
+              onChange={handleChange}
+              placeholder="Contributor PESEL"
+              value={formData.contributor.pesel}
+            />
+          </>
+        )}
+        {isAccountOwner && (
+          <>
+            <label htmlFor="name">Contributor Name:</label>
+            <span className="modal__value__noeditable" name="name">
+              {user.name}
+            </span>
+            <label htmlFor="surname">Contributor Surname:</label>
+            <span className="modal__value__noeditable" name="surname">
+              {user.surname}
+            </span>
+            <label htmlFor="address">Contributor Address:</label>
+            <span className="modal__value__noeditable" name="address">
+              {user.address}
+            </span>
+            <label htmlFor="email">Contributor Email:</label>
+            <span className="modal__value__noeditable" name="email">
+              {user.email}
+            </span>
+            <label htmlFor="phoneNumber">Contributor Phone Number:</label>
+            <span className="modal__value__noeditable" name="phoneNumber">
+              {user.phoneNumber}
+            </span>
+            <label htmlFor="pesel">Contributor PESEL:</label>
+            <span className="modal__value__noeditable" name="pesel">
+              {user.pesel}
+            </span>
+          </>
+        )}
         <button type="submit">Deposit</button>
         <button onClick={onClose}>Cancel</button>
       </form>
