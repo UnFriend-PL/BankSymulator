a5164d8e9f7f7925f04f0e08c13499872405bb75
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2024-03-14 00:11:34 +0100

    design

---
+++ banksymulatorclient/src/Components/ApplicationComponent/Application.jsx
@@ -0,0 +1,76 @@
+import { useContext, useState } from "react";
+import { NotificationContext } from "../../Providers/NotificationProvider/NotificationProvider";
+import apiService from "../../Services/ApiService";
+
+export function Application({ application, display }) {
+  const [showContent, setShowContent] = useState(false);
+  const { showNotification } = useContext(NotificationContext);
+  const handleAccept = async (e) => {
+    const response = await apiService(
+      "patch",
+      `/api/Application/AcceptApplication/${application.applicationId}/${e}`,
+      undefined,
+      true
+    );
+    if (response.success === true) {
+      showNotification(response);
+    } else {
+      showNotification(response);
+    }
+  };
+
+  return (
+    <div className="applicationWrapper__applications__application">
+      <div
+        className="applicationWrapper__applications__application__preview"
+        onClick={() => {
+          setShowContent(!showContent);
+        }}
+      >
+        <div
+          className={`applicationWrapper__applications__application__preview__status ${application.status}`}
+        >
+          {display == "Sent" ? display : application.status}
+        </div>
+        <div className="applicationWrapper__applications__application__preview__subject">
+          {application.subject}
+        </div>
+        <div className="applicationWrapper__applications__application__preview__to">
+          {application.jointName} {application.jointSurname}
+        </div>
+        <div className="applicationWrapper__applications__application__preview__date">
+          {application.sendTime.split("T")[0]}
+        </div>
+      </div>
+      {showContent && (
+        <div className="applicationWrapper__applications__application__content">
+          <div className="applicationWrapper__applications__application__content__message">
+            {application.message}
+          </div>
+          {display == "Pending" && (
+            <>
+              <div className="applicationWrapper__applications__application__content__buttons">
+                <button
+                  className="accept"
+                  onClick={() => {
+                    handleAccept(true);
+                  }}
+                >
+                  Accept
+                </button>
+                <button
+                  className="decline"
+                  onClick={() => {
+                    handleAccept(false);
+                  }}
+                >
+                  Decline
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
